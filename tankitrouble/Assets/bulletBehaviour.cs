98c3b4ef840332bf7e48b329d8e80759557154ea
vernonjudepui <vernonjudepui@yahoo.com.sg>
2022-03-03 23:45:51 +0800

    Updated ust need elims and scoring and text

---
+++ tankitrouble/Assets/bulletBehaviour.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public float lifetime = 4.0f;
+    void OnCollisionEnter2D(Collision2D col)
+    {
+
+        if (col.gameObject.name=="Bullet(Clone)"){
+            Destroy(col.gameObject);
+            Destroy(this.gameObject);
+        }
+        if(col.gameO)
+    }
+    void Awake()
+    {
+        Destroy(this.gameObject, lifetime);
+    }
+}
